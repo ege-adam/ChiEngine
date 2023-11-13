b822d252ce33e743c4f47572e375a408690dca8f
Ege <17130579+ege-adam@users.noreply.github.com>
2023-11-14 00:56:35 +0100

    Folder structure & GLFW Setup

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,19 @@ function LinkVulkan()
 	links { "$(VULKAN_SDK)/lib/vulkan-1.lib" }
 end
 
+function IncludeGLFW()
+	includedirs "Libraries/GLFW/Include"
+end
+
+function LinkGLFW()
+	libdirs "Libraries/GLFW/Lib"
+
+	-- Our static lib should not link against GLFW
+	filter "kind:not StaticLib"
+		links "glfw3"
+	filter {}
+end
+
 workspace "ChiEngine"
     configurations { "Debug", "Release" }
     location "build"
@@ -19,17 +32,23 @@ workspace "ChiEngine"
 
 	filter { }
 
+	targetdir ("Build/Bin/%{prj.name}/%{cfg.longname}")
+	objdir ("Build/Obj/%{prj.name}/%{cfg.longname}")
+
 project "ChiEngine"
 	kind "ConsoleApp"
+	files "Projects/ChiEngine/**"
 	language "C++"
 	architecture "x64"
 	location "build/ProjSetup"
 	targetdir "bin/%{cfg.buildcfg}"
 	includedirs {
-		"src"
+		"Projects/ChiEngine/"
 	}
+
 
 	IncludeVulkan()
 	LinkVulkan()
 
-	files { "src/**.h", "src/**.hpp", "src/**.cpp" }
+	IncludeGLFW()
+	LinkGLFW()
